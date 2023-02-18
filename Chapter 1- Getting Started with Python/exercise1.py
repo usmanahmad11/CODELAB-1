19b3eef071693cd4d9b3d1b0feff1086a0f29918
Usman Ahmad <usmanahmad50@outlook.com>
2023-02-18 12:14:48 +0400

    Update exercise1.py

--- Chapter 1- Getting Started with Python/exercise1.py
+++ Chapter 1- Getting Started with Python/exercise1.py
@@ -1,1 +1,10 @@
-print("This is Code Lab 1 Class ")
+print("Twinkle, twinkle, little star")
+
+print("     How I wonder what you are! ")
+
+print("        Up above the world so high,")
+print("        Like a diamond in the sky.")
+
+print("Twinkle, twinkle, little star, ")
+
+print("     How I wonder what you are")
